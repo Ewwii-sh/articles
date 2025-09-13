138fa6b75bd64c98250ac9177633ca3198f94d9b
Byson94 <byson94@outlook.com>
2025-09-13 17:27:01 +0530

    feat: added the full page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Ewwii Articles";
+export const SITE_DESCRIPTION = "Welcome to ewwii articles!";
---
+++ src/content/articles/what_is_ewwii.md
@@ -0,0 +1,8 @@
+---
+title: "What is ewwii?"
+description: "An explanation on what ewwii is and is not"
+pubDate: "Sep 08 2025"
+heroImage: "../../assets/blog-placeholder-3.jpg"
+---
+
+WIP
